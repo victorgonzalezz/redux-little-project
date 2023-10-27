9244caf2a2340f274c36352f79a1ae7658ea7e67
Victor Gonzalez <vitorpcg@gmail.com>
2023-10-27 12:08:20 +0100

    player video
    added player video

--- src/components/pages/Player.jsx
+++ src/components/pages/Player.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
-import { MessageCircle } from "lucide-react"
+import { ChevronDown, MessageCircle, Video } from "lucide-react"
+import ReactPlayer from 'react-player'
 
 export function Player() {
   return (
@@ -19,9 +20,37 @@ export function Player() {
         </div>
           <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow">
           <div className="flex-1">
-            Video
+            <div className="w-full bg-zinc-950 aspect-video">
+              <ReactPlayer
+                width="100%"
+                height="100%"
+                controls
+                url="https://www.youtube.com/watch?v=r0oDe1GyeX0"
+              />
+            </div>
           </div>
-          <aside className="w-80 border-l border-zinc-800 bg-zing-900 h-[600px]">
+          <aside className="w-80 border-l border-zinc-800 bg-zing-900">
+            <div>
+              <button className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+                <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-sm">
+                  1
+                </div>
+                <div className="flex flex-col gap-1 text-left">
+                  <strong className="text-sm">Desvendando o Redux</strong>
+                  <span className="text-sm text-zinc-400">12 Aulas</span>
+                </div>
+
+                <ChevronDown className="w-5 h-5 ml-auto text-zinc-400" />
+              </button>
+              <nav className="relative flex flex-col gap-4 p-6">
+                <button className="flex items-center gap-3 text-sm text-zinc-400">
+                  <Video className="w-4 h-4 text-zinc-500" />
+                  <span>Fundamentos do Redux</span>
+                  <span className="ml-auto font-mono text-xs text-zinc-500">09:13</span>
+                </button>
+
+              </nav>
+            </div>
 
           </aside>
           </main> 
