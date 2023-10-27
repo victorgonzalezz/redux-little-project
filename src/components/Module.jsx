1bef99247984141e5c62853ea1ae73e1d0667268
Victor Gonzalez <vitorpcg@gmail.com>
2023-10-27 17:15:53 +0100

    radix-ui
    used radix-ui to get the functional component without styling

--- src/components/Module.jsx
+++ src/components/Module.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
+import * as Collapsible from '@radix-ui/react-collapsible';
 import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
 
 export function Module({moduleIndex, title, amountOfLessons}) {
   return (
-    <div>
-      <button className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+    <Collapsible.Root className='group'>
+      <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
         <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-sm">
           {moduleIndex + 1}
         </div>
@@ -14,9 +15,10 @@ export function Module({moduleIndex, title, amountOfLessons}) {
           <span className="text-sm text-zinc-400">{amountOfLessons}</span>
         </div>
 
-        <ChevronDown className="w-5 h-5 ml-auto text-zinc-400" />
-      </button>
+        <ChevronDown className="w-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
+      </Collapsible.Trigger>
 
+      <Collapsible.Content>
       <nav className="relative flex flex-col gap-4 p-6">
         <Lesson
           title="Fundamentos do Redux"
@@ -31,7 +33,8 @@ export function Module({moduleIndex, title, amountOfLessons}) {
           duration="03:33"
         />
        </nav>
-
-    </div>
+      </Collapsible.Content>
+      
+    </Collapsible.Root>
   )
 }
